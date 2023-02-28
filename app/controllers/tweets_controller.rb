77ee5acad891ece3237364cbf2fbb1de86a5b7e9
sabanonitsuke <sabakan.nari@gmail.com>
2023-02-28 22:04:23 +0900

    重複したアクションをまとめる(show, edit)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+
   def index
     @tweets = Tweet.all
   end
@@ -15,7 +17,6 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
@@ -24,11 +25,15 @@ class TweetsController < ApplicationController
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
   end
 
   private
+
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
